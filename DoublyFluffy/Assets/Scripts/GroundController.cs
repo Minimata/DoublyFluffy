6574f49037ce5868433ed57551bdf80d39a2725b
Alexandre Serex <alexandre.serex@he-arc.ch>
2017-06-30 23:13:40 +0200

    Added ground prefabs

---
+++ DoublyFluffy/Assets/Scripts/GroundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour
+{
+
+    [SerializeField] private float m_speed = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        Vector3 move = Vector3.down * m_speed;
+	    transform.position += move;
+	}
+}
