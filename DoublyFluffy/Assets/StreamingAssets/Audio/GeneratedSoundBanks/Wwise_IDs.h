177fe1a9dbb6b7fdc8b1e3f088ed9e298439e9de
Zirkiel <v.personeni@bluewin.ch>
2017-07-01 15:12:50 +0200

    Wwise soundbank update INSANE

--- DoublyFluffy/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ DoublyFluffy/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,51 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID GAME_OVER_TOO_FAST = 775889684U;
+        static const AkUniqueID GAME_OVER_TOO_SLOW = 957044181U;
+        static const AkUniqueID MUSIC_START = 3725903807U;
+        static const AkUniqueID STATE_DOWN = 3579758495U;
+        static const AkUniqueID STATE_UP = 489003304U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYER_LIFE
+        {
+            static const AkUniqueID GROUP = 3762137787U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD_FAST = 3079415640U;
+                static const AkUniqueID DEAD_SLOW = 1398680609U;
+            } // namespace STATE
+        } // namespace PLAYER_LIFE
+
+        namespace STATE_JUICYINESS
+        {
+            static const AkUniqueID GROUP = 1617566071U;
+
+            namespace STATE
+            {
+                static const AkUniqueID STATE_0 = 1277114381U;
+                static const AkUniqueID STATE_1 = 1277114380U;
+                static const AkUniqueID STATE_2 = 1277114383U;
+                static const AkUniqueID STATE_3 = 1277114382U;
+                static const AkUniqueID STATE_4 = 1277114377U;
+            } // namespace STATE
+        } // namespace STATE_JUICYINESS
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
